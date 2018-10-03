eb2152a35b26aef5f76102255dc63310de21f9af
SpitfireMK10 <petervdm9@gmail.com>
2018-10-03 13:25:46 +0200

    Second update

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject oceanTile;
+    public int size;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int x =0; x<size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                Instantiate(oceanTile, new Vector3(x, 0, y), Quaternion.identity);
+            }
+        }
+	}
+}
